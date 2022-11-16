14519ef92f46d46abec5b23e51d6026f500cb05a
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2022-11-17 01:11:30 +0530

    structure

---
+++ DarkDevsWebsite/public/pages/index.js
@@ -0,0 +1,6 @@
+const index = () => {
+    return <h1>Home</h1>;
+  };
+  
+export default index;
+  
---
+++ DarkDevsWebsite/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Index from './pages/index';
+import Navbar from './pages/nav';
+import Footer from './pages/footer';
+function App() {
+  return (
+    <div className="App">
+          < Navbar/>
+          < Index />
+          < Footer/>
+    </div>
+  );
+}
+
+export default App;
