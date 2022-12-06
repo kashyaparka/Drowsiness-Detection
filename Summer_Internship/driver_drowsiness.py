de6d4258fd86cde2ddfc04b1406c6d25a30d7164
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2022-12-06 13:42:35 +0530

    Changes made

--- Summer_Internship/driver_drowsiness.py
+++ Summer_Internship/driver_drowsiness.py
@@ -4,13 +4,15 @@ import cv2
 import numpy as np
 # Dlib for deep learning based Modules and face landmark detection
 import dlib
+import winsound
 #face_utils for basic operations of conversion
 from imutils import face_utils
 
 
 #Initializing the camera and taking the instance
 cap = cv2.VideoCapture(0)
-
+freq = 440
+duration = 1000
 #Initializing the face detector and landmark detector
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
@@ -23,28 +25,28 @@ status=""
 color=(0,0,0)
 
 def compute(ptA,ptB):
-	dist = np.linalg.norm(ptA - ptB)
-	return dist
+    dist = np.linalg.norm(ptA - ptB)
+    return dist
 
 def blinked(a,b,c,d,e,f):
-	up = compute(b,d) + compute(c,e)
-	down = compute(a,f)
-	ratio = up/(2.0*down)
+    up = compute(b,d) + compute(c,e)
+    down = compute(a,f)
+    ratio = up/(2.0*down)
 
-	#Checking if it is blinked
-	if(ratio>0.25):
-		return 2
-	elif(ratio>0.21 and ratio<=0.25):
-		return 1
-	else:
-		return 0
+    #Checking if it is blinked
+    if(ratio>0.25):
+        return 2
+    elif(ratio>0.21 and ratio<=0.25):
+        return 1
+    else:
+        return 0
 
 
 while True:
     _, frame = cap.read()
     face_frame = frame.copy()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    description = "National Institute of Technology SIlchar"
     faces = detector(gray)
     #detected face in faces array
     for face in faces:
@@ -61,46 +63,50 @@ while True:
 
         #The numbers are actually the landmarks which will show eye
         left_blink = blinked(landmarks[36],landmarks[37], 
-        	landmarks[38], landmarks[41], landmarks[40], landmarks[39])
+            landmarks[38], landmarks[41], landmarks[40], landmarks[39])
         right_blink = blinked(landmarks[42],landmarks[43], 
-        	landmarks[44], landmarks[47], landmarks[46], landmarks[45])
+            landmarks[44], landmarks[47], landmarks[46], landmarks[45])
         
         #Now judge what to do for the eye blinks
         if(left_blink==0 or right_blink==0):
-        	sleep+=1
-        	drowsy=0
-        	active=0
-        	if(sleep>6):
-        		status="SLEEPING !!!"
-        		color = (255,0,0)
+            sleep+=1
+            drowsy=0
+            active=0
+            if(sleep>6):
+                status="SLEEPING !!!"
+                color = (255,0,0)
+                winsound.Beep(freq,duration)
 
         elif(left_blink==1 or right_blink==1):
-        	sleep=0
-        	active=0
-        	drowsy+=1
-        	if(drowsy>6):
-        		status="Drowsy !"
-        		color = (0,0,255)
+            sleep=0
+            active=0
+            drowsy+=1
+            if(drowsy>6):
+                status="Drowsy !"
+                color = (0,0,255)
 
         else:
-        	drowsy=0
-        	sleep=0
-        	active+=1
-        	if(active>6):
-        		status="Active :)"
-        		color = (0,255,0)
-        	
-        cv2.putText(frame, status, (100,100), cv2.FONT_HERSHEY_SIMPLEX, 1.2, color,3)
+            drowsy=0
+            sleep=0
+            active+=1
+            if(active>6):
+                status="Active :)"
+                color = (0,255,0)
+            
+        cv2.putText(frame, status, (450,450), cv2.FONT_HERSHEY_SIMPLEX, 1.2, color,3)
 
         for n in range(0, 68):
-        	(x,y) = landmarks[n]
-        	cv2.circle(face_frame, (x, y), 1, (255, 255, 255), -1)
-
+            (x,y) = landmarks[n]
+            cv2.circle(face_frame, (x, y), 1, (255, 255, 255), -1)
+    cv2.putText(frame, "Driver Drowsiness Detection", (30,30), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,255,255),1)
+    cv2.putText(frame, "Electrical Engineering Department", (30,50), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,255,255),1)
+    cv2.putText(frame, description, (30,70), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,255,255),1)
+    cv2.putText(frame, "Under the supervision of the Dr. Risha Mal,Asist. Prof", (30,450), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,255,255),1)
     cv2.imshow("Frame", frame)
     cv2.imshow("Result of detector", face_frame)
     key = cv2.waitKey(1)
     if key == 27:
-      	break
+          break
 
 
 
